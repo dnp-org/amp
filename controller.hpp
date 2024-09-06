63e3b840d3948bf3fc692e6d3c192d56f4334569
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2024-09-06 16:46:19 -0400

    remove old hpp files

--- controller.hpp
+++
@@ -1,8 +0,0 @@
-struct Controller
-{
-    Certificate identity_certificate_;
-    optional<Certificate> authority_certificate_;
-    vector<Association> associations_;
-    Policies policies_;
-};
-
